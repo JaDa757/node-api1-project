b519cf581cc37a5069cb1febe142d729c17a71fb
Joshua Ackerman <joshackerman757@gmail.com>
2023-11-16 13:45:11 -0500

    proper error message on wrong id

--- api/server.js
+++ api/server.js
@@ -1,33 +1,37 @@
-const express = require('express'); 
+const express = require('express');
 const User = require('./users/model')
 const server = express()
 
 server.get('/api/users', (req, res) => {
     User.find()
-    .then(users => {
-        res.json(users)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message: 'error getting users',
-            err: err.message,
-            stack: err.stack
+        .then(users => {
+            res.json(users)
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack: err.stack
+            })
+        })
 })
 
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
-    .then(user => {
-        console.log(user)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message:'error getting user',
-            err: err.message,
-            stack: err.stack,
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist",
+                })
+            }
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting user',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
 })
 
 
